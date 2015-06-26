bd7949dd75ec8cbaafc6142f76f1e2c5af417df0
curdriceaurora <sra@outlook.com>
2015-06-26 13:42:38 -0700

    Assignment 7 Completed

--- BlocExercises/BlocExercises/WizardOfOz.m
+++ BlocExercises/BlocExercises/WizardOfOz.m
@@ -12,12 +12,13 @@
 
 - (BOOL) switchesFromBlackAndWhiteToColor {
     /* WORK HERE */
-    return NO;
+    return YES;
 }
 
 - (NSString *) mainCharacter {
     /* WORK HERE */
-    return @"";
+    //Added "Dorothy" to make testTheMainCharacterIsCorrectPass
+    return @"Dorothy";
 }
 
 @end
