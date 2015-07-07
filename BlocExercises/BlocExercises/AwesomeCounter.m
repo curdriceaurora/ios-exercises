ca453d66c402979b0e15a332916de078ab7aaaa4
curdriceaurora <sra@outlook.com>
2015-07-07 13:14:45 -0700

    Assignment 12 completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,35 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSInteger smallerNumber,largerNumber;
+    if (number==otherNumber)
+    {
+        NSString *equalString = [NSString stringWithFormat:@"%ld",number];
+        return equalString;
+    }
+    
+    else if(number < otherNumber)
+    {
+        smallerNumber = number;
+        largerNumber = otherNumber;
+    }
+    else
+        {
+            smallerNumber = otherNumber;
+            largerNumber = number;
+        }
+    
+    NSString *stringWithNumbersBetweenNumber = @"";
+    for ( NSInteger n=smallerNumber;n <= largerNumber;n++)
+    {
+        NSLog(@"n is now: %ld", (long)n);
+        NSString *integerAsString = [NSString stringWithFormat: @"%ld", n];
+        stringWithNumbersBetweenNumber = [stringWithNumbersBetweenNumber stringByAppendingString:integerAsString];
+        NSLog(@"n is now: %@",integerAsString);
+        NSLog(@"String is now:%@",stringWithNumbersBetweenNumber);
+    }
+    
+    return stringWithNumbersBetweenNumber;
 }
 
 @end
