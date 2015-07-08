3e42f14dc105b5485568125972487f637ee7ffa6
curdriceaurora <sra@outlook.com>
2015-07-07 21:51:04 -0700

    Assignment 13 completed Arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,40 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    NSArray *characterArray = [characterString componentsSeparatedByString:@";"];
+    return characterArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    NSString *characterString = [characterArray componentsJoinedByString:@";"];
+
+    return characterString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *myMutableCharacterArray = [NSMutableArray arrayWithArray:characterArray];
+    
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [myMutableCharacterArray sortUsingDescriptors:@[sortDescriptor]];
+    return myMutableCharacterArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
+    NSMutableArray *myMutableCharacterArrayContainsWorf = [NSMutableArray arrayWithArray:characterArray];
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [myMutableCharacterArrayContainsWorf filterUsingPredicate:containsWorf];
+    
+    if(myMutableCharacterArrayContainsWorf.count==0)
+    {
+        return NO;
+    }
+    else
+        return YES;
+    
 }
 
 @end
