f406b9c4bc963a781c1917cc6e531e8d5a22e0fd
curdriceaurora <sra@outlook.com>
2015-09-30 19:23:34 -0700

    Assignment 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -7,22 +7,57 @@
 //
 
 #import "NumberHandler.h"
+#define DOUBLER 2 //couldnt figure out how to use typedef for integers
 
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    NSInteger m = [number integerValue]*DOUBLER;
+    NSNumber *p= [NSNumber numberWithInteger:m];
+    return p;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSInteger smallerNumber,largerNumber;
+    if (number==otherNumber)
+    {
+    
+        
+    }
+
+    else if(number < otherNumber)
+    {
+        smallerNumber = number;
+        largerNumber = otherNumber;
+    }
+    else
+    {
+        smallerNumber = otherNumber;
+        largerNumber = number;
+    }
+    __block NSMutableArray *intermediateArray = [NSMutableArray array];
+    
+     for ( NSInteger n=smallerNumber;n <= largerNumber;n++)
+        
+        
+    {
+        NSLog(@"n is now: %ld", (long)n);
+        [intermediateArray addObject:[NSNumber numberWithInt:n]];
+
+    }
+    
+    return intermediateArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    NSArray *sortedArray;
+    sortedArray = [arrayOfNumbers sortedArrayUsingSelector:@selector(compare:)];
+    NSNumber *lowestNumber = [sortedArray objectAtIndex:(0)];
+    NSInteger lowestInteger =[lowestNumber integerValue];
+    
+    return lowestInteger;
 }
 
 @end
