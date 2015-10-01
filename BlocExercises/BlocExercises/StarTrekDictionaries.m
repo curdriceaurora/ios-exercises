afdad6c8160c1f2bd75905d3c3ca9ccf54cd7eb7
curdriceaurora <sra@outlook.com>
2015-09-30 17:46:46 -0700

    Assignment 14 completed

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,32 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    id favoriteDrink = characterDictionary[@"favorite drink"];
+    NSString *favDrink ;
+    if (favoriteDrink != nil && [favoriteDrink isKindOfClass:[NSString class]]) {
+        NSLog(@"Favorite Movie: %@", favoriteDrink);    /* WORK HERE */
+        favDrink = favoriteDrink;}
+    return favDrink;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+     NSArray *intermediateDrinksArray = [charactersArray valueForKeyPath:@"favorite drink"];
+    return intermediateDrinksArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    NSMutableDictionary *tempDictionary;
+    
+    tempDictionary = [[NSMutableDictionary alloc]initWithDictionary:characterDictionary];
+    [tempDictionary setObject:@"Things are only impossible until they're not!" forKey:@"quote"];
+    
+//    [tempDictionary enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+//        NSLog(@"In our dictionary, the key \"%@\" corresponds with %@", key, obj);
+//    }];
+    
+    return tempDictionary;
 }
 
 @end
